1cb8424857aade79c95a726c4a6fa09aab178de0
Zakia Afrin Jeme <zjeme@techlaunch.io>
2018-09-20 11:12:13 -0400

    Update

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
 const boxes = document.querySelectorAll(".col")
 
-for(let i=0; i<boxes.length; i++){
+for (let i = 0; i < boxes.length; i++) {
     boxes[i].textContent = i + 1;
 }
 const colors = [
-    'rgb(255, 255, 51)',
-    'rgb(255, 0, 43)',
-    'rgb(43, 255, 0)',
-    'rgb(25, 255, 255)',
-    'rgb(153, 0, 153)'
+    'rgb(255, 0, 102)', //0 
+    'rgb(153, 0, 153)', //1
+    'rgb(255, 0, 0)', //2
+    'rgb(0, 204, 0)', //3
+    'rgb(255, 0, 102)' //4
 ]
 //defining the class
-class NumberBoxes{
-    constructor(colors){
+class NumberBoxes {
+    constructor(colors) {
         //the constructor should run as soon as i create a new instances
         // this.changeNumber();
         // this.changeColor();
         this.colors = colors;
         this.addClickHandlers();
     }
-    changeNumber(num){
+    changeNumber(num) {
         //console.log("Running changeNumber")
         //console.log(num)
         //first loop and DESEND(itterate down not up) starting fromthe num that was first in
-        for(let i=num; i>0; i--){
+        for (let i = num; i > 0; i--) {
             //grab the textcontent from the box(parseInt changes the string value into a number value
             //so it can be incremented)
-            let value = parseInt(boxes[i-1].textContent);
+            let value = parseInt(boxes[i - 1].textContent);
             //then increment by 1 
-            value = (value >= 9)? 1 : value + 1;
+            value = (value >= 9) ? 1 : value + 1;
             //then put it back in the box
-            boxes[i-1].textContent = value;
+            boxes[i - 1].textContent = value;
         }
 
     }
-    changeColor(num){
+    changeColor(num) {
         //console.log("Running changeColor")
         //first loop and DESEND(itterate down not up) starting from the num that was passed in
         // let style = window.getComputedStyle(boxes[num-1])
         // console.log(style.backgroundColor)
-        for(let i=num; i>0; i--){
+        for (let i = num; i > 0; i--) {
             //i need to get the computed style of my element
-            let style = window.getComputedStyle(boxes[i-1])
+            let style = window.getComputedStyle(boxes[i - 1])
             //using the style that i just computed i will search through the colors array to find the index of the boxes
             //current color
             let index = this.colors.indexOf(style.backgroundColor)
@@ -52,11 +52,11 @@ class NumberBoxes{
         }
     }
     //method to add click handlers on each box
-    addClickHandlers(){
+    addClickHandlers() {
         //boxes is outside the class you are going to loop over every element of this nodelist...
         boxes.forEach(box => {
             //..then add a event listener on each one
-            box.addEventListener('click', e =>{
+            box.addEventListener('click', e => {
                 //then run the following functions
                 this.changeNumber(box.dataset.place);
                 this.changeColor(box.dataset.place);
